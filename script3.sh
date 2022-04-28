0c4abd7d25b6f7da3e81057bdef52188d9e3687b
SCBUILD DEV <67809938+Alaminbd257@users.noreply.github.com>
2022-04-29 00:47:06 +0300

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -3,9 +3,9 @@ cp /usr/share/zoneinfo/Asia/Dubai /etc/localtime
 
 #Database Details
 db_host='172.105.250.34';
-db_user='scvpnapp_yoyop';
-db_pass='@@AlaMinbd27';
-db_name='scvpnapp_yoyop';
+db_user='scvpnapp_vipbossvpn';
+db_pass='@@AlaMinbd25';
+db_name='scvpnapp_vipbossvpn';
 
 install_require()
 {
